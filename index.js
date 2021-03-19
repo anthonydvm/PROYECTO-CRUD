8444663a66327c532e8fed8c166ad4c37bfa0225
Lenovo <anthony.victoria95@gmail.com>
2021-03-19 16:53:41 -0500

    Agregando funcionalidad a index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {  
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const btn = document.getElementById('add');
+
+    btn.onclick = function () {
+        console.log('Title:', title.value, 'Descripction:', description.value);
+    }
+})
